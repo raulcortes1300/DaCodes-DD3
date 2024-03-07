4ce13c77b9997c84e52516e3e342286841f7472c
raulcortes1300 <raulcortes1300@users.noreply.github.com>
2024-03-06 23:28:19 -0600

    Remove commented unused AppIcon component from NavBar

--- src/components/NavBar/NavBar.component.tsx
+++ src/components/NavBar/NavBar.component.tsx
@@ -14,23 +14,6 @@ const NavBar = () => {
         height="60px"
         alt="App Logo"
       />
-      {/* <div>
-        <AppIcon
-          onClick={() => "bye"}
-          imgUrl="https://cdn0.iconfinder.com/data/icons/art-designing-glyph/2048/1871_-_Magnifier-512.png"
-          width="60px"
-          height="60px"
-          alt="Search Event Icon"
-        />
-      </div> */}
-      {/* <div
-        style={{ width: "50px" }}
-        className="border border-green-500 !important"
-      >
-        Hi there
-      </div>
-      <div className="border border-pink-500">Hi there</div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
     </div>
   );
 };
